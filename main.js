fbaa8ef1dc5cc2df1e7de4cef08e04d24b61db1b
Heather Olcot <hfolcot@gmail.com>
2019-07-11 14:49:24 +0100

    implemented sequencing

--- main.js
+++ main.js
@@ -1,28 +1,54 @@
-(function($){
+(function ($) {
 
-    $.fn.extend({ 
+    $.fn.extend({
 
-        addTemporaryClass: function(className, duration) {
+        addTemporaryClass: function (className, duration) {
             var elements = this;
-            setTimeout(function() {
+            setTimeout(function () {
                 elements.removeClass(className);
             }, duration);
 
-            return this.each(function() {
+            return this.each(function () {
                 $(this).addClass(className);
             });
         }
+
     });
 
 })(jQuery);
 
-$(document).ready(function() {
-    $('#green');
-    $('#red');
-    $('#blue');
-    $('#yellow');
+var sequence = []
+var sequenceCount = 0;
+function playSequence(sequence, sequenceCount) {
+    setTimeout(function () {
+        $('#' + sequence[sequenceCount]).addTemporaryClass('lit', 500);
+        sequenceCount++;
+        if (sequenceCount < sequence.length) {
+            playSequence(sequence, sequenceCount)
+        }
+        console.log(sequence);
+        console.log(sequenceCount)
+    },  700)
+
+}
+
+function getNext() {
+    number = Math.random() * 4;
+    sequence.push(Math.ceil(number));
+    playSequence(sequence, sequenceCount);
+}
+
+$(document).ready(function () {
+    $('#1');
+    $('#2');
+    $('#3');
+    $('#4');
     $('.circle').click(function () {
         $(this).addTemporaryClass('lit', 500);
+        getNext();
+    })
+    $('#go').click(function () {
+        getNext();
     })
 });
 
