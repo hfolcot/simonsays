f4388be31c5d07123e7e2448fee3eef57a2d39bc
Heather Olcot <hfolcot@gmail.com>
2019-07-11 14:13:10 +0100

    can press buttons

---
+++ main.js
@@ -0,0 +1,30 @@
+(function($){
+
+    $.fn.extend({ 
+
+        addTemporaryClass: function(className, duration) {
+            var elements = this;
+            setTimeout(function() {
+                elements.removeClass(className);
+            }, duration);
+
+            return this.each(function() {
+                $(this).addClass(className);
+            });
+        }
+    });
+
+})(jQuery);
+
+$(document).ready(function() {
+    $('#green');
+    $('#red');
+    $('#blue');
+    $('#yellow');
+    $('.circle').click(function () {
+        $(this).addTemporaryClass('lit', 500);
+    })
+});
+
+
+
