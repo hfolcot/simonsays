5c6c3bb43104dbf9d25b1dbe25bec2a3c16be404
Heather <hfolcot@gmail.com>
2019-07-14 14:01:51 +0100

    better folder structure

---
+++ assets/js/main.js
@@ -0,0 +1,94 @@
+(function ($) {
+
+    $.fn.extend({
+
+        addTemporaryClass: function (className, duration) {
+            var elements = this;
+            setTimeout(function () {
+                elements.removeClass(className);
+            }, duration);
+
+            return this.each(function () {
+                $(this).addClass(className);
+            });
+        }
+
+    });
+
+})(jQuery);
+
+var playerSequence = []
+var sequence = []
+var sequenceCount = 0;
+var sound1 = new Audio('assets/sounds/1.wav')
+var sound2 = new Audio('assets/sounds/2.wav')
+var sound3 = new Audio('assets/sounds/3.wav')
+var sound4 = new Audio('assets/sounds/4.wav')
+soundList = [sound1, sound2, sound3, sound4]
+function playSequence(sequence, sequenceCount) {
+    setTimeout(function () {
+        var current = sequence[sequenceCount]
+        $('#' + current).addTemporaryClass('lit', 500);
+        soundList[current - 1].play();
+        sequenceCount++;
+        if (sequenceCount < sequence.length) {
+            playSequence(sequence, sequenceCount)
+        }
+    },  700)
+    playerSequence = [];
+}
+
+function getNext() {
+    number = Math.random() * 4;
+    sequence.push(Math.ceil(number));
+    playSequence(sequence, sequenceCount);
+    
+}
+
+
+function endGame() {
+    $('#messages').html("You lose! Your score was " + (sequence.length -1) + "!");
+}
+
+function checkForMatch(pseq) {
+    for (i=0; i<pseq.length; i++) {
+        if (pseq[i] != sequence[i]) {
+            endGame();
+        }
+        if (pseq.length === sequence.length && i === (sequence.length - 1)) {
+            getNext();
+        }
+    }
+    return true;
+}
+
+
+$(document).ready(function () {
+    $('#go').click(function () {
+        $('#messages').html("");
+        getNext();
+    })
+    $('#reset').click(function () {
+        playerSequence = [];
+        sequence = [];
+        sequenceCount = 0;
+        $('#messages').html("");
+    })
+    $('.circle').click(function () {
+        if (sequence.length > 0) {
+            $(this).addTemporaryClass('lit', 500);
+            soundList[($(this).attr('id')) -1].pause();
+            soundList[($(this).attr('id')) -1].load();
+            soundList[($(this).attr('id')) -1].play();
+            playerSequence.push(parseInt($(this).attr('id')));
+            checkForMatch(playerSequence);
+        } else {
+            $('#messages').html("Press play to begin!")
+        }
+        
+    
+    })
+});
+
+
+
