020b672f567f4defb79f9291f4ea92093b282149
Heather <hfolcot@gmail.com>
2019-07-14 11:50:21 +0100

    added sounds

--- main.js
+++ main.js
@@ -17,11 +17,20 @@
 
 })(jQuery);
 
+var playerSequence = []
 var sequence = []
 var sequenceCount = 0;
+var sound1 = new Audio('sounds/1.wav')
+var sound2 = new Audio('sounds/2.wav')
+var sound3 = new Audio('sounds/3.wav')
+var sound4 = new Audio('sounds/4.wav')
+soundList = [sound1, sound2, sound3, sound4]
 function playSequence(sequence, sequenceCount) {
     setTimeout(function () {
-        $('#' + sequence[sequenceCount]).addTemporaryClass('lit', 500);
+        var current = sequence[sequenceCount]
+        console.log(current)
+        $('#' + current).addTemporaryClass('lit', 500);
+        soundList[current - 1].play();
         sequenceCount++;
         if (sequenceCount < sequence.length) {
             playSequence(sequence, sequenceCount)
@@ -38,18 +47,24 @@ function getNext() {
     playSequence(sequence, sequenceCount);
 }
 
+function playerGo() {
+    $('.circle').click(function () {
+        $(this).addTemporaryClass('lit', 500);
+        playerSequence.push(parseInt($(this).attr('id')));
+        console.log(playerSequence);
+    })
+}
+
 $(document).ready(function () {
     $('#1');
     $('#2');
     $('#3');
     $('#4');
-    $('.circle').click(function () {
-        $(this).addTemporaryClass('lit', 500);
-        getNext();
-    })
+    
     $('#go').click(function () {
         getNext();
     })
+    playerGo();
 });
 
 
